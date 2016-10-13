bf70998a6830ddf7899de78e239a07da57741805
Sean Moroney <spm61@case.edu>
2016-10-12 22:37:48 -0400

    ITS ALIVE!  And Integerated into one directory.  AND the textures load.  All in a night's work.

--- spm61-Forge/MinecraftCircuits/src/main/java/minecraftcircuits/CommonProxy.java
+++ spm61-Forge/MinecraftCircuits/src/main/java/minecraftcircuits/CommonProxy.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.player.EntityPlayer;
 /**
  * CommonProxy is used to set up the mod and start it running.  It contains all the code that should run on both the
  *   Standalone client and the dedicated server.
- *   For more background information see here http://greyminecraftcoder.blogspot.com/2013/11/how-forge-starts-up-your-code.html
  */
 public abstract class CommonProxy {
 
--- spm61-Forge/MinecraftCircuits/src/main/java/minecraftcircuits/DedicatedServerProxy.java
+++ spm61-Forge/MinecraftCircuits/src/main/java/minecraftcircuits/DedicatedServerProxy.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.player.EntityPlayerMP;
 /**
  * DedicatedServerProxy is used to set up the mod and start it running on dedicated servers.  It contains all the code that should run on the
  *   dedicated servers.  This is almost never required.
- *   For more background information see here http://greyminecraftcoder.blogspot.com/2013/11/how-forge-starts-up-your-code.html
  */
 public class DedicatedServerProxy extends CommonProxy
 {
