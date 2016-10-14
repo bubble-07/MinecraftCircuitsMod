24236a873d2292c4b517ef083ed9859faf74db99
Alexander Grabanski <ajg137@case.edu>
2016-10-14 16:59:28 -0400

    Untrack .metadata directory, remove extraneous output fields on circuits

--- circuitsMod/src/main/java/com/circuits/circuitsmod/circuitImplementation/AndCircuit.java
+++ circuitsMod/src/main/java/com/circuits/circuitsmod/circuitImplementation/AndCircuit.java
@@ -1,22 +1,18 @@
 package com.circuits.circuitsmod.circuitImplementation;
 
-import com.circuits.circuitsmod.reflective.*;
 public class AndCircuit {
 	
 	boolean input1;
 	boolean input2;
-	boolean output;
 	
 	public AndCircuit() {
 		input1 = false;
 		input2 = false;
-		output = false;
 	}
 	
 	 void tick(boolean i1, boolean i2) {
 		 input1 = i1;
 		 input2 = i2;
-		 output = value0();
 	 }
 	 
 	 boolean value0() {
--- circuitsMod/src/main/java/com/circuits/circuitsmod/circuitImplementation/XorCircuit.java
+++ circuitsMod/src/main/java/com/circuits/circuitsmod/circuitImplementation/XorCircuit.java
@@ -4,18 +4,15 @@ public class XorCircuit {
 
 	boolean input1;
 	boolean input2;
-	boolean output;
 	
 	public XorCircuit() {
 		input1 = false;
 		input2 = false;
-		output = false;
 	}
 	
 	 void tick(boolean i1, boolean i2) {
 		 input1 = i1;
 		 input2 = i2;
-		 output = value0();
 	 }
 	 
 	 boolean value0() {
